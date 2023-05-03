2de946eb974bd25768993afc80c84a2fc638f3c3
biswarupmurmu <murmubiswarup@gmail.com>
2023-05-03 10:24:05 +0530

    added profile view (public)

--- linkta/views.py
+++ linkta/views.py
@@ -1,6 +1,7 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
+from .models import User
 
 views = Blueprint("views",__name__)
 
@@ -9,3 +10,11 @@ def home():
     if g.user:
         return render_template("profile/profile.html", user = g.user)
     return render_template("home/home.html")
+
+@views.route("/<username>")
+def profile_public(username):
+    user =  User.query.filter_by(username=username).first()
+    if user:
+        return render_template("profile/profile_public.html", user = user)
+    else:
+        return f"<p>No user named <strong>{username}</strong></p>"
