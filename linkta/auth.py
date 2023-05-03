444c1a2e64e8d6a4a14172ad766f984d85cecfdc
biswarupmurmu <murmubiswarup@gmail.com>
2023-05-03 09:11:58 +0530

    added login functionality

--- linkta/auth.py
+++ linkta/auth.py
@@ -36,3 +36,47 @@ def signup():
             flash("Account created successfully", category="success")
 
     return render_template("forms/signup.html")
+
+# login
+@auth.route("/login", methods = ["GET", "POST"])
+def login():
+    if request.method == "POST":
+
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user =  User.query.filter_by(email=email).first()
+
+        if user:
+            if not check_password_hash(user.password, password):
+                flash("Incorrect password",category="error")
+            else:
+                session.clear()
+                session['user_id'] = user.id
+                flash(f"You are logged in as {user.username} successfully", category="success")
+                return redirect(url_for("views.home"))
+        else:
+            flash("No account exixts for this email address", category="error")
+
+    return render_template("forms/login.html")
+
+# stay logged in
+@auth.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.filter_by(id=user_id).first()
+
+# log in required
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+
+        return view(**kwargs)
+
+    return wrapped_view
--- linkta/views.py
+++ linkta/views.py
@@ -1,9 +1,11 @@
 from flask import (
-    Blueprint, render_template
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
 views = Blueprint("views",__name__)
 
 @views.route("/")
 def home():
+    if g.user:
+        return render_template("profile/profile.html", user = g.user)
     return render_template("home/home.html")
