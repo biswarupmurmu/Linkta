fc92516f6dcb68497c6aa14c4d3fe578c512b672
biswarupmurmu <murmubiswarup@gmail.com>
2023-05-04 10:36:43 +0530

    added edit profile functionality

--- linkta/views.py
+++ linkta/views.py
@@ -2,6 +2,8 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from .models import User
+from .auth import login_required
+from . import db
 
 views = Blueprint("views",__name__)
 
@@ -19,3 +21,36 @@ def profile_public(username):
         return render_template("profile/profile_public.html", user = user)
     else:
         return f"<p>No user named <strong>{username}</strong></p>"
+    
+@views.route("/edit", methods = ['GET','POST'])
+@login_required
+def profile_edit():
+    if request.method == 'POST':
+        new_contact_email = request.form.get("contact_email")
+        new_username = request.form.get("username").lower()
+        new_fname = request.form.get("fname")
+        new_lname = request.form.get("lname")
+        new_bio = request.form.get("bio")
+        public_view = request.form.get("public_view")
+        if public_view:
+            public_view = True
+        else:
+            public_view = False
+
+        username_exists = None
+        if new_username != g.user.username:
+            username_exists = User.query.filter_by(username=new_username).first()
+            if username_exists:
+                flash("Username already exists", category="error")
+
+        if not username_exists:
+            g.user.contact_email = new_contact_email
+            g.user.username = new_username
+            g.user.fname = new_fname
+            g.user.lname = new_lname
+            g.user.bio = new_bio
+            g.user.public_view = public_view
+            db.session.commit()
+            flash("Account updated successfully", category="success")
+
+    return render_template("profile/profile_edit.html", user = g.user)
