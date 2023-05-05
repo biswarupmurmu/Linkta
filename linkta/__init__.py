2ca9de2118bf457e11ceb94f61715c5a54d5feed
biswarupmurmu <murmubiswarup@gmail.com>
2023-05-05 23:06:34 +0530

    UPLOAD folder

--- linkta/__init__.py
+++ linkta/__init__.py
@@ -1,15 +1,32 @@
 from flask import Flask
+import os
 
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
+
+UPLOAD_FOLDER = os.path.join('linkta/static', 'user_uploads')
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+        SQLALCHEMY_DATABASE_URI = "sqlite:///database.db",
+        MAX_CONTENT_LENGTH = 16 * 1000 * 1000,
+        UPLOAD_FOLDER = UPLOAD_FOLDER
     )
     app.config.from_pyfile('config.py', silent=True)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    # ensure the UPLOAD folder exists
+    try:
+        os.makedirs(UPLOAD_FOLDER)
+    except OSError:
+        pass
 
     from .views import views
     from .auth import auth
